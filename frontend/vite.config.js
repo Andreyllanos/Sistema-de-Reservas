9772e57fc31765dd6ea84b3653954ccd5437e8af
Andreyllanos <andreyllanos023@gmail.com>
2026-06-26 11:25:18 -0500

    Usar serve como servidor de producción

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,4 +2,11 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 export default defineConfig({
     plugins: [react()],
+    preview: {
+        host: '0.0.0.0',
+        port: 8080,
+        allowedHosts: [
+            'sistema-de-reservas-production-72e3.up.railway.app'
+        ],
+    },
 });
